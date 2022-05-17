8cfd981f8334c0d9cb483645de39ad7d49d07328
shibli21 <syedshiblimahmud@gmail.com>
2022-05-17 15:55:44 +0600

    added hello world api

---
+++ HelloWorld/Controllers/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get() => Ok("Hello World!");
+
+        [HttpPost]
+        public ActionResult<string> Post(string payload) => Ok(payload);
+    }
+}
