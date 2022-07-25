3f62c9152c44a7dfc09b22974bb759fd85fb0458
shibli21 <syedshiblimahmud@gmail.com>
2022-07-25 21:56:17 +0600

    online users

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,24 @@
+import { User } from './../users/model/user';
+import { UsersService } from './../users/users.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  onlineUsers?: User[];
 
-  constructor() { }
+  constructor(private usersService: UsersService) {}
 
   ngOnInit() {
+    this.usersService.getOnlineUsers().subscribe((users) => {
+      this.onlineUsers = users;
+    });
+    console.log(this.onlineUsers);
+    this.usersService.setUserOnline().subscribe((data) => {
+      console.log(data);
+    });
   }
-
 }
