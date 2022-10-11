be76525a37ae4f96f4679e1b16f9c889273f4d1f
shibli21 <syedshiblimahmud@gmail.com>
2022-10-11 18:42:57 +0600

    ✨ comment section

---
+++ twitter-clone/src/app/tweet/tweet-comment/tweet-comment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tweet-comment',
+  templateUrl: './tweet-comment.component.html',
+  styleUrls: ['./tweet-comment.component.scss']
+})
+export class TweetCommentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- twitter-clone/src/app/tweet/tweet.module.ts
+++ twitter-clone/src/app/tweet/tweet.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TweetComponent } from './tweet.component';
 import { SharedModule } from '../shared/shared.module';
+import { TweetCommentComponent } from './tweet-comment/tweet-comment.component';
+import { NewCommentComponent } from './new-comment/new-comment.component';
 
 @NgModule({
-  declarations: [TweetComponent],
+  declarations: [TweetComponent, TweetCommentComponent, NewCommentComponent],
   imports: [CommonModule, TweetRoutingModule, SharedModule],
 })
 export class TweetModule {}
