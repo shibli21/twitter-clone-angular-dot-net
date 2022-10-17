d32321f73efd97db8b23436dd6aef2215b8baa13
masumbillah3416 <masumbillahsujon1998@gmail.com>
2022-10-18 03:40:01 +0600

    retweeted count added in Tweet Model

--- Core/Dtos/TweetResponseDto.cs
+++ Core/Dtos/TweetResponseDto.cs
@@ -10,6 +10,7 @@ namespace Core.Dtos
         public string Tweet { get; set; } = string.Empty;
         public long CommentCount { get; set; } = 0;
         public long LikeCount { get; set; } = 0;
+        public long RetweetCount { get; set; } = 0;
         public string[] History { get; set; } = new string[0];
         public DateTime CreatedAt { get; set; } = DateTime.Now;
 
