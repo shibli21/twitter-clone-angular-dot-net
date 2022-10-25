581e289a8be8829982e9d223443bab8172648e51
shibli21 <syedshiblimahmud@gmail.com>
2022-10-25 20:56:13 +0600

    fix timeline

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,27 @@
 import { PaginatedTweets } from './../core/models/tweet.model';
 import { TimelineService } from './../core/services/timeline.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   newsFeed!: PaginatedTweets | null;
   isLoading = false;
   constructor(private timelineService: TimelineService) {}
+
+  ngOnDestroy(): void {
+    this.timelineService.newsFeed.next({
+      tweets: [],
+      page: 0,
+      totalPages: 0,
+      lastPage: 0,
+      size: 0,
+      totalElements: 0,
+    });
+  }
 
   ngOnInit(): void {
     this.timelineService.isLoadingNewsFeed.subscribe((isLoading) => {
