2f6fc3f15fde6787be479cb812651ec62f809c54
shibli21 <syedshiblimahmud@gmail.com>
2022-10-26 02:43:59 +0600

    fix input

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
+    extend: {
+      boxShadow: {
+        inputRed: ["0 0 0 1px #ef4444"],
+        input: ["0 0 0 1px #6366f1"],
+      },
+    },
     screens: {
       xs: "475px",
       ...defaultTheme.screens,
