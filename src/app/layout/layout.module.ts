a922de9fadb7ee933bf78e0c7d5dcf3d0d25dd8e
shibli21 <syedshiblimahmud@gmail.com>
2022-11-03 01:39:13 +0600

    remove junk

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from './../shared/shared.module';
 import { FollowersCardComponent } from './followers-card/followers-card.component';
-import { NavComponent } from './nav/nav.component';
 import { UserLayoutComponent } from './user-layout/user-layout.component';
 import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
 import { AdminNavComponent } from './admin-nav/admin-nav.component';
@@ -13,7 +12,6 @@ import { BottomNavComponent } from './bottom-nav/bottom-nav.component';
 @NgModule({
   declarations: [
     UserLayoutComponent,
-    NavComponent,
     FollowersCardComponent,
     AdminLayoutComponent,
     AdminNavComponent,
