4f557c8e946bbf8a1a223cef859f8ea5e7521344
shibli21 <syedshiblimahmud@gmail.com>
2022-11-05 22:47:40 +0600

    login form updated

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -12,7 +12,7 @@ import { AuthService } from './../auth.service';
 })
 export class LoginComponent implements OnInit {
   loginForm = new FormGroup({
-    email: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required]),
   });
   isLoading: boolean = false;
