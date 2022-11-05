c679433d8d18e27b160c9fb2b559aeaf349bc09f
shibli21 <syedshiblimahmud@gmail.com>
2022-11-05 13:27:04 +0600

    admin nav  updated

--- src/app/shared/icons/tweet-icon/tweet-icon.component.ts
+++ src/app/shared/icons/tweet-icon/tweet-icon.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tweet-icon',
   templateUrl: './tweet-icon.component.html',
 })
 export class TweetIconComponent implements OnInit {
+  @Input() fillClass = '';
+
   constructor() {}
 
   ngOnInit(): void {}
