2eb434907f8f86429b0b527c99ce6e2a08b38305
shibli21 <syedshiblimahmud@gmail.com>
2022-12-23 16:10:14 +0600

    design updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
-  important: true,
   theme: {
     extend: {
       fontFamily: {
